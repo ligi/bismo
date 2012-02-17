27849ae341e5884350998a487db619a944fd3bbf
ligi <ligi@ligi.de>
2012-02-17 20:02:35 +0100

    preparing showing of the barcode to connect

--- tv/src/org/bismo/tv/BismoTVActivity.java
+++ tv/src/org/bismo/tv/BismoTVActivity.java
@@ -2,12 +2,20 @@ package org.bismo.tv;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.provider.Settings.Secure;
+import android.widget.ImageView;
 
 public class BismoTVActivity extends Activity {
-    /** Called when the activity is first created. */
+    
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        Secure.getString(getApplicationContext().getContentResolver(),Secure.ANDROID_ID);
+        
         setContentView(R.layout.main);
+        
+        ImageView img_v=(ImageView)this.findViewById(R.id.barcode_img);
     }
 }
