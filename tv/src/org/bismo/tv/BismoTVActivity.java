a9cc1ca5a28ad1f91c95d6d69baedfee8c4af62a
ligi <ligi@ligi.de>
2012-02-17 20:15:42 +0100

    milestone #1 showing barcode with device_id

--- tv/src/org/bismo/tv/BismoTVActivity.java
+++ tv/src/org/bismo/tv/BismoTVActivity.java
@@ -1,5 +1,8 @@
 package org.bismo.tv;
 
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.provider.Settings.Secure;
@@ -12,10 +15,17 @@ public class BismoTVActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        Secure.getString(getApplicationContext().getContentResolver(),Secure.ANDROID_ID);
+        
+        ImageLoader img_loader= ImageLoader.getInstance();
+        img_loader.init(ImageLoaderConfiguration.createDefault(this));
+        
+        String device_id=Secure.getString(getApplicationContext().getContentResolver(),Secure.ANDROID_ID);
         
         setContentView(R.layout.main);
         
         ImageView img_v=(ImageView)this.findViewById(R.id.barcode_img);
+        
+        img_loader.displayImage("http://chart.apis.google.com/chart?cht=qr&chs=350x350&chld=L&choe=UTF-8&chl=http%3A%2F%2Fbismoapp.appspot.com/tv/"+device_id, img_v);
+        
     }
 }
