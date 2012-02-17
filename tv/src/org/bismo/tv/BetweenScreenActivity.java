41ad0fea67a484ac97dd78c0cab5e7ec6d0e0a80
ligi <ligi@ligi.de>
2012-02-17 22:12:22 +0100

    Implement Pause Indicator ProgressBar

--- tv/src/org/bismo/tv/BetweenScreenActivity.java
+++ tv/src/org/bismo/tv/BetweenScreenActivity.java
@@ -4,13 +4,17 @@ import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.Settings.Secure;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 
 public class BetweenScreenActivity extends BaseActivity {
     
-	public final static int PAUSE_TIME=3000; // in ms
+	public final static int PAUSE_TIME=30000; // in ms
+	private ProgressBar progress;
+	private long pause_start;
+	private Handler hndl;
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,10 +31,37 @@ public class BetweenScreenActivity extends BaseActivity {
         
         img_loader.displayImage("http://chart.apis.google.com/chart?cht=qr&chs=350x350&chld=L&choe=UTF-8&chl=http%3A%2F%2Fbismoapp.appspot.com/tv/"+device_id, img_v);
 
-        ProgressBar progress=(ProgressBar)findViewById(R.id.progress_bar);
-        progress.setMax(100);
+        progress=(ProgressBar)findViewById(R.id.progress_bar);
+        progress.setMax(PAUSE_TIME);
+        
+        pause_start=System.currentTimeMillis();
+        
+        hndl=new Handler();
+        
+        new Thread(new ProgressUpdaterThread()).start();
 	}
 
+	class ProgressUpdaterThread implements Runnable {
+		
+		@Override
+		public void run() {
+			while ((System.currentTimeMillis()-pause_start)<PAUSE_TIME) {
+				try {
+					Thread.sleep(10);
+				} catch (InterruptedException e) {		}
+				
+				hndl.post(new ProgressUpdater());
+			}	
+		}
+
+		
+	}
+	class ProgressUpdater implements Runnable {
+		@Override
+		public void run() {
+			progress.setProgress(progress.getMax()-(int)(System.currentTimeMillis()-pause_start));
+		}
+	}
 	
     
 }
