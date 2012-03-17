2bbcea6bb192c7f640575f38d4b68cc9cd8efc6a
ligi <ligi@ligi.de>
2012-03-17 14:40:58 +0100

    rm the action behind boot for now ( until setting )

--- tv/src/org/bismo/tv/BootReceiver.java
+++ tv/src/org/bismo/tv/BootReceiver.java
@@ -8,9 +8,9 @@ public class BootReceiver extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		Intent start_i=new Intent(context,BismoTVActivity.class);
-		start_i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-		context.startActivity(start_i);
+		//Intent start_i=new Intent(context,BismoTVActivity.class);
+		//start_i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		//context.startActivity(start_i);
 	}
 
 }
