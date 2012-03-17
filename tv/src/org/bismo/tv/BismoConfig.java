72fe3dc9458e5dc373cc5170a3b55b3fb234de5f
ligi <ligi@ligi.de>
2012-03-17 14:41:47 +0100

    backend in config

---
+++ tv/src/org/bismo/tv/BismoConfig.java
@@ -0,0 +1,8 @@
+package org.bismo.tv;
+
+public class BismoConfig {
+
+	public final static String SERVER_URL="https://bismobackend.appspot.com";
+	public final static String HTTP_SERVER_URL="http://bismobackend.appspot.com";
+
+}
